7ec87dc4171c499597937fcd1345080067be9c70
TheScrawl <thescrawlgaming@gmail.com>
2018-05-21 23:42:43 +0000

    made everything good and built a house

--- ncpyrpg.py
+++ ncpyrpg.py
@@ -1,22 +1,70 @@
 import curses
+import math
 from curses import wrapper
 
-def mapControl(screen, mapArray):
-	#Draw Map
+array = [	'its a house',	
+					'###############',
+					'#             #',
+					'#             #',
+					'#             #',
+					'#             #',
+					'####### #######', ]
+def mapControl(window, array):
+	x = 2
+	y = 2
 	for i in array:
-		screen.addstr(y, x, i)
-		y = y + 1
-
-	playerX = 1
-	playerY = 1
+		window.addstr(y, x, i)
+		y = y + 1	
+		
 	
-	
+	playerX = 4
+	playerY = 4
+		
+	curses.noecho()
+	curses.cbreak()
+	curses.curs_set(False)  
+	curses.use_default_colors()
+	window.keypad(True)
 
+	while True:
+		keypress = window.getch()
+		try:
+			if keypress == ord('w'):
+				if chr(window.inch(playerY - 1, playerX)) == ' ':
+					window.addch(playerY, playerX, ' ')
+					window.addstr(playerY - 1, playerX, u'o')
+					playerY = playerY - 1
+				else: 
+					pass
+			if keypress == ord('s'):
+				if chr(window.inch(playerY + 1, playerX)) == ' ':
+					window.addch(playerY, playerX, ' ')
+					window.addstr(playerY + 1, playerX, u'o')
+					playerY = playerY + 1 
+				else:
+					pass
+			if keypress == ord('a'):
+				if chr(window.inch(playerY, playerX - 1)) == ' ':
+					window.addch(playerY, playerX, ' ')
+					window.addstr(playerY, playerX - 1, u'o')
+					playerX = playerX - 1     
+				else:
+					pass
+			if keypress == ord('d'):
+				if chr(window.inch(playerY, playerX + 1)) == ' ':
+					window.addch(playerY, playerX, ' ')
+					window.addstr(playerY, playerX + 1, u'o') 
+					playerX = playerX + 1   
+				else:
+					pass
+		except(curses.error, ValueError):
+			pass
 def main(masterWindow):
-	masterWindow = curses.initscr()
-	maxHeight = masterWindow.getmaxyx()[0]
-	maxWidth = masterWindow.getmaxyx()[1]
-	mapWindow = masterWindow.derwin(int(maxHeight / 2), maxWidth, 5, 5)
+	stdscr = curses.initscr()
+	maxHeight, maxWidth = stdscr.getmaxyx()
+	mapWindow = curses.newwin(int(math.ceil(maxHeight / 2)) , int(math.ceil(maxWidth)), 0, 0)
 	while True:
 		mapWindow.box()  
+		mapWindow.refresh()
+		mapControl(mapWindow, array)
 wrapper(main)
